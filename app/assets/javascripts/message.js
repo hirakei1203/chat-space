5ff65ef8ec07ec70d42f2a20a82577b55a19a314
hirakei1203 <56023847+hirakei1203@users.noreply.github.com>
2019-10-21 20:28:59 +0900

    auto update 1st

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -19,6 +19,75 @@ $(function(){
                 return html;
   }
 
+  let reloadMessages = function() {
+    last_message_id = params[:id].to_i
+    $.ajax({
+      url: "api/messages",
+      type: 'get',
+      dataType: 'json',
+      data: {id: last_message_id}
+    })
+    .done(function(messages) {
+      console.log('success');
+    })
+    .fail(function() {
+      console.log('error');
+    });
+  };
+
+  let buildMessageHTML = function(message) {
+    if (message.content && message.image.url) {
+       `let html = <div class="message" data-id = ${message.id}>
+         <div class="upper-message">
+           <div class="upper-message__user-name">
+            ${message.user_name}
+           </div>
+           <div class="upper-message__date">
+            &{message.created_at}
+           </div>
+         </div>
+         <div class="lower-message">
+           <p class="lower-message__content">
+            ${message.content}
+           </p> 
+           <img src=${message.image.url}, class="lower-message__image" >
+         </div> 
+       </div>`
+    } else if (message.content) {
+      `var html = <div class="message" data-id=${message.id}>
+         <div class="upper-message">
+           <div class="upper-message__user-name">
+            ${message.user_name}
+           </div>
+           <div class="upper-message__date">
+            ${message.created_at}
+           </div>
+         </div>
+         <div class="lower-message">
+           <p class="lower-message__content">
+            ${message.content}
+           </p>
+         </div>
+       </div>`
+    } else if (message.image.url) {
+      var html = `<div class="message" data-id=${message.id}>
+         <div class="upper-message"> 
+           <div class="upper-message__user-name">
+            ${message.user_name} 
+           </div>
+           <div class="upper-message__date"> 
+            ${message.created_at} 
+           </div>
+         </div>
+         <div class="lower-message"> 
+          <img src="${message.image.url}, "class="lower-message__image" >
+        </div>
+      </div>`
+    };
+    return html;
+  };
+
+
   $('#form__switch').on('submit',function(e){
     e.preventDefault();
     let formData = new FormData(this);
@@ -44,5 +113,26 @@ $(function(){
       $('.form__submit').prop('disabled', false);
     })
   });
+
+  var reloadMessages = function() {
+    last_message_id = params[:id].to_i
+    $.ajax({
+      url: "group_messages",
+      type: 'get',
+      dataType: 'json',
+      data: {id: last_message_id}
+    })
+    .done(function(messages) {
+      let insertHTML = 'buildMessageHTML';
+      $('').append(insertHTML);
+     
+
+
+    })
+    .fail(function() {
+      console.log('error');
+    });
+  };
+
 
 });
